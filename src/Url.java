f94fcad05d276d60da521e566f4dfc53a95eabd0
Af0nso-duarte <114400307+Af0nso-duarte@users.noreply.github.com>
2024-03-11 14:36:43 +0000

    Add files via upload

---
+++ src/Url.java
@@ -0,0 +1,57 @@
+import java.io.Serializable;
+
+public class Url implements Serializable {
+    private int id;
+    private String url;
+    private String title;
+    private String citation;
+    
+    public Url(int id, String url, String title, String citation) {
+        this.id = id;
+        this.url = url;
+        this.title = title;
+        this.citation = citation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCitation() {
+        return citation;
+    }
+
+    public void setCitation(String citation) {
+        this.citation = citation;
+    }
+
+    @Override
+    public String toString() {
+        return "Url{" +
+                "id=" + id +
+                ", url='" + url + '\'' +
+                ", title='" + title + '\'' +
+                ", citation='" + citation + '\'' +
+                '}';
+    }
+}
